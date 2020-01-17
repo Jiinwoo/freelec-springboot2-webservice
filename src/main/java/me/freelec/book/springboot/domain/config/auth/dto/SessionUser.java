018719845a08a8b03021e2534fac8665d2744edc
jinwoojung <jwjung5038@gmail.com>
2020-01-17 23:43:40 +0900

    naver , google로그인 추가

---
+++ src/main/java/me/freelec/book/springboot/domain/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package me.freelec.book.springboot.domain.config.auth.dto;
+
+import lombok.Getter;
+import me.freelec.book.springboot.domain.user.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/me/freelec/book/springboot/web/IndexController.java
+++ src/main/java/me/freelec/book/springboot/web/IndexController.java
@@ -1,12 +1,17 @@
 package me.freelec.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.freelec.book.springboot.domain.config.auth.LoginUser;
+import me.freelec.book.springboot.domain.user.User;
 import me.freelec.book.springboot.service.PostsService;
 import me.freelec.book.springboot.web.dto.PostsResponseDto;
+import me.freelec.book.springboot.domain.config.auth.dto.SessionUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,12 +19,15 @@ public class IndexController {
 
     private final PostsService postsService;
 
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
-
     }
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id , Model model){
