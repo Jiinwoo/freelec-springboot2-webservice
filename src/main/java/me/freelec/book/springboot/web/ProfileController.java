1234521f724ee5deea8f526e199dbb775b83ffb3
jinwoojung <jwjung5038@gmail.com>
2020-01-19 01:48:43 +0900

    profile 테스트시 oauth 적용되었던 문제 해결

--- src/main/java/me/freelec/book/springboot/web/ProfileController.java
+++ src/main/java/me/freelec/book/springboot/web/ProfileController.java
@@ -15,14 +15,14 @@ public class ProfileController {
     private final Environment env;
 
     @GetMapping("/profile")
-    public String profile(){
+    public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real","real1","real2");
-        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        return profiles.stream().filter(realProfiles::contains)
+        return profiles.stream()
+                .filter(realProfiles::contains)
                 .findAny()
                 .orElse(defaultProfile);
     }
-
 }
