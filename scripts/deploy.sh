6a47a7c49073215b24aec38b6c310f9d41cd4768
jinwoojung <jwjung5038@gmail.com>
2020-01-18 23:53:44 +0900

    deploy.sh 프로젝트 명 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=freelect-springboot2-webservice
+PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 
@@ -13,7 +13,7 @@ CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print 
 
 echo "현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"] ; then
+if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
